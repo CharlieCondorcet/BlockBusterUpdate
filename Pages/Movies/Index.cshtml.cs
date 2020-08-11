117b4d68293b829deb6b5a1d2d8dca57185b12a8
CharlieCondorcet <charliecondorcet@gmail.com>
2020-08-11 09:04:40 -0400

    Add search: update the generated CRUD and index to search

--- Pages/Movies/Index.cshtml.cs
+++ Pages/Movies/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Blockbuster.Data;
 using Blockbuster.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BlockbusterUpdate.Pages.Movies
 {
@@ -19,11 +20,31 @@ namespace BlockbusterUpdate.Pages.Movies
             _context = context;
         }
 
-        public IList<Movie> Movie { get;set; }
+        public IList<Movie> Movie { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+
+        // Requires using Microsoft.AspNetCore.Mvc.Rendering;
+        public SelectList Genres { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            // using System.Linq;
+            var movies = from m in _context.Movie select m;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => 
+                s.Title.Contains(SearchString));
+            }
+
+            Movie = await movies.ToListAsync(); 
+
+            //Movie = await _context.Movie.ToListAsync();
         }
     }
 }
